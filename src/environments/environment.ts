0f7fb01c97517007348f5052dd7578b31ea74e90
nsbrajapaksha <nsbrajapaksha93@gmail.com>
2019-03-04 20:40:57 +0530

    commited

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  //production: false,
 
   firebase: {
     apiKey: 'AIzaSyC5_3qN17U4GB0CokOHe4Lkql3GWcPK3SQ',
